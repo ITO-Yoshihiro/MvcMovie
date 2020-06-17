d796dd9badb1b05fe1310a100964df2cb31bde0a
ito0032 <ito.yoshihiro@ad-max.co.jp>
2020-06-18 08:54:28 +0900

    データベースの操作まで実装

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -26,11 +26,19 @@ namespace MvcMovie.Controllers
         // GET: /Helloworld/Welcome/
         // Requires using System.Text.Encodings.Web;
         //public string Welcome(string name, int numTimes = 1)
-        public string Welcome(string name, int ID = 1)
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    //return "This is the Welcome action method...";
+        //    //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        //}
+
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            //return "This is the Welcome action method...";
-            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
