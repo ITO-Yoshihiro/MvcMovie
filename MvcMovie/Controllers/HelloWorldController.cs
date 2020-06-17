2cca9ec9870c2c96580158b151dcb6ae3e16b9ea
ito0032 <ito.yoshihiro@ad-max.co.jp>
2020-06-17 22:35:27 +0900

    プロジェクト ファイルを追加します。

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        //
+        // GET: /Helloworld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        //public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
+        {
+            //return "This is the Welcome action method...";
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
